2b5c43b09a134d8f10eeafb4809d97ae4b62b55b
Hoty <bsavchu1@stu.vistula.edu.pl>
2026-04-21 18:43:28 +0200

    Java backend was launch and connection with html. Templates for html was added

--- src/main/java/com/web/book/BookApplication.java
+++ src/main/java/com/web/book/BookApplication.java
@@ -1,13 +1,13 @@
-package com.web.project7736377277;
+package com.web.book;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Project7736377277Application {
+public class BookApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Project7736377277Application.class, args);
+        SpringApplication.run(BookApplication.class, args);
     }
 
 }
---
+++ src/main/java/com/web/book/BookRepository.java
@@ -0,0 +1,7 @@
+package com.web.book;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookRepository {
+}
