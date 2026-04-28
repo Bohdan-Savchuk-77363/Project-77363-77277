9790bd9435a467a692632c1553667ac2df19fe72
OlexanderPaduh-77277 <aleksandrpaduh7@gmail.com>
2026-04-28 09:20:08 +0200

    new directory structure

---
+++ src/main/java/com/web/controller/BookController.java
@@ -0,0 +1,43 @@
+package com.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller 
+public class BookController {
+
+    @GetMapping("/") 
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/about") 
+    public String about() {
+        return "about"; 
+    }
+
+    @GetMapping("/books") 
+    public String books() {
+        return "books"; 
+    }
+
+    @GetMapping("/ebooks") 
+    public String ebooks() {
+        return "ebooks"; 
+    }
+
+    @GetMapping("/catalog") 
+    public String catalog() {
+        return "catalog"; 
+    }
+
+    @GetMapping("/favourite") 
+    public String favourite() {
+        return "favourite"; 
+    }
+
+    @GetMapping("/account") 
+    public String account() {
+        return "account"; 
+    }
+}
