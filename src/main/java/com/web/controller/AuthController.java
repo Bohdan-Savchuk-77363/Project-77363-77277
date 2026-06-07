2904c8946d985071d039d8527e18a3bb060187cb
OlexanderPaduh-77277 <aleksandrpaduh7@gmail.com>
2026-06-07 12:00:20 +0200

    Login and registration are completed. Ready to start build google books api handler.

--- src/main/java/com/web/controller/AuthController.java
+++ src/main/java/com/web/controller/AuthController.java
@@ -3,9 +3,13 @@ package com.web.controller;
 import com.web.entity.User;
 import com.web.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/auth")
@@ -18,14 +22,40 @@ public class AuthController {
         this.userService = userService;
     }
 
+
     @PostMapping("/register")
     public String register(@ModelAttribute User user, Model model) {
         try {
             userService.register(user);
             return "redirect:/login"; // после регистрации → на логин
+
         } catch (IllegalArgumentException e) {
             model.addAttribute("error", e.getMessage());
             return "public/authorization/registration-page"; // назад с ошибкой
         }
     }
-}
+
+    @PostMapping("/login")
+    public String login(@RequestParam String email, @RequestParam String password, Model model){
+        try{
+            User user = new User();
+            user.setEmail(email);
+//            user.setName();
+            user.setPassword(password);
+            userService.logginig(user, password);
+            User loggedUser = userService.logginig(user, password);
+
+            UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(loggedUser.getName(), null, List.of());
+            SecurityContextHolder.getContext().setAuthentication(auth);
+
+            return "/account";
+        }catch (IllegalArgumentException e){
+            model.addAttribute("error", e.getMessage());
+            return "public/authorization/login-page";
+        }
+    }
+
+
+
+
+    }
--- src/main/java/com/web/service/UserService.java
+++ src/main/java/com/web/service/UserService.java
@@ -1,22 +1,23 @@
 package com.web.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import com.web.entity.User;
 import com.web.repository.UserRepository;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @Transactional
-public class UserService {
+public class    UserService {
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
     }
     public void register(User user){
         if (user.getEmail() == null || user.getEmail().isBlank()){
@@ -31,9 +32,30 @@ public class UserService {
         if (userRepository.existsByEmailIgnoreCase(user.getEmail())) {
             throw new IllegalArgumentException("same email already exist");
         }
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-
+//        user.setPassword(user.getPassword());
         userRepository.save(user);
 
     }
+    public User logginig(User user, String password){
+
+        if (user.getEmail() == null || user.getEmail().isBlank()){
+
+            throw new IllegalArgumentException("email is empty!!!");
+        }
+        if (user.getPassword() == null || user.getPassword().length() < 6) {
+            throw new IllegalArgumentException("password must have at list 6 symbol");
+        }
+        User foundUser = userRepository.findByEmailIgnoreCase(user.getEmail()).orElseThrow(() -> new  IllegalArgumentException("User not found"));
+
+        if(!password.equals(foundUser.getPassword())){
+            throw new IllegalArgumentException("Wrong password");
+        }
+        return foundUser;
+
+
+    }
+
+
+
+
 }
