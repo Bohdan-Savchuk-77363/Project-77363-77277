cdf28e48a73bc464bcab21e794c5b07c0c9b5e95
OlexanderPaduh-77277 <aleksandrpaduh7@gmail.com>
2026-06-10 13:28:49 +0200

    returned books file code

--- src/main/java/com/web/controller/BookController.java
+++ src/main/java/com/web/controller/BookController.java
@@ -5,8 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller 
+@Controller
 public class BookController {
+    private final BookService bookService;
 
     public BookController(BookService bookService) {
         this.bookService = bookService;
@@ -14,7 +15,8 @@ public class BookController {
 
     @GetMapping("/")
     public String bookData(Model model) {
-        model.addAttribute("books", BookSe.getA)
+        System.out.println("Count of page: " + bookService.getAllBook().size());
+        model.addAttribute("books", bookService.getAllBook());
 
         return "index";
     }
